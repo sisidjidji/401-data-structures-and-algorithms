c128840d18c7f4ea7c634579d47b35db019bc26c
sihem azibi <s.azibi@live.fr>
2020-05-17 20:50:06 -0500

    puch function and tests runing

---
+++ Data-Structures-testing/stacktests.cs
@@ -0,0 +1,64 @@
+﻿using Data_Structures;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Data_Structures_testing
+{
+   public class stacktests
+    {
+        [Fact]
+        public void peek_empty_stack_throw()
+        {
+            //arrange
+            Stack<int> stack = new Stack<int>();
+
+            //act
+            //assert
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+
+        }
+
+        [Fact]
+        public void can_puch_one_item()
+        {
+            //arrange
+            Stack<int> stack = new Stack<int>();
+
+            //act
+
+              stack.Push(5);
+          
+            //assert
+            Assert.NotNull(stack);
+
+
+        }
+        [Fact]
+        public void can_puch_multiple_items()
+        {
+            //arrange
+            Stack<int> stack = new Stack<int>();
+
+            //act
+
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+
+            //assert
+            Assert.Contains(1,stack);
+            Assert.Contains(2, stack);
+            Assert.Contains(3, stack);
+            Assert.Contains(4, stack);
+            Assert.Contains(5, stack);
+        
+
+        }
+    }
+}
--- Data-Structures/stacks/stack.cs
+++ Data-Structures/stacks/stack.cs
@@ -1,10 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Data_Structures.staks
+namespace Data_Structures.stakes
 {
-    class stack
+    public class Stack<T>
+
     {
+        private Node<T> top;
+        public T Push(T value)
+        {
+            Node<T>  newNode = new Node<T>(value) { Next = top };
+            top = newNode;
+            
+                return top.Value;
+
+        }
+        public T Pop()
+        {
+
+            return default;
+        }
+
+        public T Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return default;
+        }
+        private class Node<T>
+        {
+            public Node(T value)
+            {
+                Value = value;
+             
+            }
+
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+        }
+
     }
-}
+    }
+
+
