4299b3cd1b995688cc5aa5b5b1457db05f436a6f
sihem azibi <s.azibi@live.fr>
2020-05-18 01:43:25 -0500

    queus functions runing

--- Data-Structures/Queue/Queue.cs
+++ Data-Structures/Queue/Queue.cs
@@ -5,32 +5,76 @@ namespace Data_Structures.Queue
 {
     public class Queue<T>
     {
-       private  Node <T> front ;
-       private Node  <T> rear;
+       private Node <T> front ;
+
+       
 
         public T EnQueus(T value)
         {
             Node<T> newNode = new Node<T>(value);
-            newNode.Next = rear;
-            rear = newNode;
 
-            return newNode.Value;
+            if (front == null)
+            {
+                 front=newNode;
+
+                return newNode.Value;
+
+            }
+            
+                front.Next = newNode;
+           
+                return front.Value;
         }
 
-     private class Node<T>
+        public T DeQueus()
         {
-            public Node<T>Front { get; set; }
-            public Node<T> Rear { get; set; }
+            if (front == null)
+            {
+                throw new InvalidOperationException();
+            }
+            Node <T> dequeue = front;
+            front=front.Next;
+            dequeue.Next = null;
+
+            return dequeue.Value;
+        }
+
+
+        public T Peek()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException();
+            }
+            
+
+            return front.Value;
+        }
+
+
+        public bool IsEmpty()
+        {
+            if (front == null)
+            {
+                return true;
+            }
+            else
+            return false;
+        }
+
+        private class Node<T>
+        {
+            
+           
             public T Value { get; set; }
             
             public Node <T>Next { get; set; }            
-
+         
             public Node(T value)
             {
-               Value = value;
-                Rear = null;
-               Front = null;
-           
+                Value = value;
+                Next = null;
+                 
             }
         }
     }
