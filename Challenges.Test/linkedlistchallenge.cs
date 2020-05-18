78163d18fd229794040257dfda10210bec3fa5e2
sihem azibi <s.azibi@live.fr>
2020-05-17 23:35:30 -0500

    enqueus function done

---
+++ Challenges.Test/linkedlistchallenge.cs
@@ -0,0 +1,59 @@
+﻿using Data_Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Data_Structures.LinkedList;
+
+namespace Challenges
+{
+    public class Linkedlistchallenge
+    {
+
+
+        public static LinkedList MergeList(LinkedList list1, LinkedList list2)
+        {
+            Node current1 = list1.Head;
+            Node current2 = list2.Head;
+
+            if (current1 == null)
+            {
+                return list2;
+            }
+
+            else if (current2 == null)
+            {
+                return list1;
+            }
+
+
+            while (current1 != null)
+            {
+
+                Node curent1next = current1.Next;
+                Node current2next = current2.Next;
+
+                current2.Next = current1.Next;
+                current1 = current2.Next;
+
+                current1 = curent1next;
+                current2 = current2next;
+
+                if (curent1next == null)
+                {
+                    current1.Next = current2next.Next;
+                }
+
+                else if (current2next == null)
+                {
+                    current2.Next = curent1next.Next;
+                }
+
+
+
+            }
+            return list1;
+        }
+
+    }
+
+}
---
+++ Data-Structures/Queue/Queue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Data_Structures.Queue
+{
+    public class Queue<T>
+    {
+       private  Node <T> front ;
+       private Node  <T> rear;
+
+        public T EnQueus(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            newNode.Next = rear;
+            rear = newNode;
+
+            return newNode.Value;
+        }
+
+     private class Node<T>
+        {
+            public Node<T>Front { get; set; }
+            public Node<T> Rear { get; set; }
+            public T Value { get; set; }
+            
+            public Node <T>Next { get; set; }            
+
+            public Node(T value)
+            {
+               Value = value;
+                Rear = null;
+               Front = null;
+           
+            }
+        }
+    }
+}
