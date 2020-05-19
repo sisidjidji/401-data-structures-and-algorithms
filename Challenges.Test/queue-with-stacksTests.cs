4085bc24a306b0da8df3c3d8c01f47a23105facd
sihem azibi <s.azibi@live.fr>
2020-05-18 23:55:58 -0500

    enqueue done

--- Challenges.Test/queue-with-stacksTests.cs
+++ Challenges.Test/queue-with-stacksTests.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Data_Structures.stacks;
+using System;
 using System.Text;
 
 namespace Data_Structures_testing
 {
+
     class queue_with_stacksTests
     {
+
+
+
+
     }
 }
--- Challenges/queue-with-stacks/queue-with-stacks.cs
+++ Challenges/queue-with-stacks/queue-with-stacks.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace Data_Structures.queue_with_stacks
 {
-    class queue_with_stacks
+    class queue_with_stacks<T>
     {
+
+        Stack<T> stack1 = new Stack<T>();
+        Stack<T> stack2 = new Stack<T>();
+
+        public void EnQueueStackQueue(T value)
+        {
+           
+                stack1.Push(value);
+
+        }
+
+
     }
 }
