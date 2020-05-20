a8973611034f90424a98fd79ca950de80a6e0734
sihem azibi <s.azibi@live.fr>
2020-05-19 23:39:31 -0500

    created new branch

---
+++ Challenges.Test/AnimalShelterTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.Test
+{
+   public  class AnimalShelterTests
+    {
+
+        [Fact]
+        public void can_enque_animal()
+        {
+
+
+        }
+    }
+}
