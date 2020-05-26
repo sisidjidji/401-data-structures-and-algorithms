c03d9b21c38c79e591051d00a53a6b1d041aa565
sihem azibi <s.azibi@live.fr>
2020-05-26 17:29:19 -0500

    breadth function working

--- Challenges/FizzBuzzTree.cs
+++ Challenges/FizzBuzzTree.cs
@@ -8,28 +8,28 @@ namespace Challenges
     public class FizzBuzzTree<T>
     {
         public Node<T> Root;
-        public BinaryTree<string> FizzBuzz (Node<string> node)
-        {
-            BinaryTree<string> tree = new BinaryTree<string>();
+       // public BinaryTree<string> FizzBuzz (Node<string> node)
+        //{
+        //    BinaryTree<string> tree = new BinaryTree<string>();
 
-            tree.PreOrder(node);
+        //    tree.PreOrder(node);
 
-            if (Convert.ToInt32(node.Value) % 3 == 0 && Convert.ToInt32(node.Value) % 5==0)
-            {
-                node.Value = "FizzBuzz";
+        //    if (Convert.ToInt32(node.Value) % 3 == 0 && Convert.ToInt32(node.Value) % 5==0)
+        //    {
+        //        node.Value = "FizzBuzz";
                 
-            }
-            if (Convert.ToInt32(node.Value) % 3 == 0 )
-            {
-                node.Value = "Fizz";
-            }
-            if (Convert.ToInt32(node.Value) % 3 == 0 && Convert.ToInt32(node.Value) % 5 == 0)
-            {
-                node.Value = "Buzz";
-            }
+        //    }
+        //    if (Convert.ToInt32(node.Value) % 3 == 0 )
+        //    {
+        //        node.Value = "Fizz";
+        //    }
+        //    if (Convert.ToInt32(node.Value) % 3 == 0 && Convert.ToInt32(node.Value) % 5 == 0)
+        //    {
+        //        node.Value = "Buzz";
+        //    }
 
-            return tree;
-        }
+        //    return tree;
+        //}
 
     }
 }
--- Data-Structures-testing/BinaryTreeTests.cs
+++ Data-Structures-testing/BinaryTreeTests.cs
@@ -68,6 +68,24 @@ namespace Data_Structures_testing
             Assert.Equal(new[] { 12, 1, 10, 7 ,5 }, tree.PostOrder(tree.Root));
         }
 
+        [Fact]
+        public void breadth_first_test()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Node<int> node1 = new Node<int>(5);
+            Node<int> node2 = new Node<int>(10);
+            Node<int> node3 = new Node<int>(7);
+            Node<int> node4 = new Node<int>(12);
+            Node<int> node5 = new Node<int>(1);
+
+            tree.Root = node1;
+            node1.Left = node2;
+            node1.Right = node3;
+            node2.Left = node4;
+            node2.Right = node5;
+
+            Assert.Equal(new[] {5,10,7,12,1}, tree.BreadthFirst());
+        }
     }
 
 
