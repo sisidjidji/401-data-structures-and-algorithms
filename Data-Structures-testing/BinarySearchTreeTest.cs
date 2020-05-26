d5b6c58a5eedccf676c3a92e18cf2bdf841dc536
sihem azibi <s.azibi@live.fr>
2020-05-25 21:19:42 -0500

    can add to singlr root left and right passing

--- Data-Structures-testing/BinarySearchTreeTest.cs
+++ Data-Structures-testing/BinarySearchTreeTest.cs
@@ -22,5 +22,21 @@ namespace Data_Structures_testing
             tree.AddNodeToTree(5);
             Assert.NotNull(tree);
         }
+        [Fact]
+        public void can_Add_to_Single_root_node()
+        {
+            //Arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+     
+            //assert
+            tree.AddNodeToTree(5);
+            tree.AddNodeToTree(1);
+            tree.AddNodeToTree(7);
+
+            Assert.NotNull(tree.Root.Left);
+            Assert.NotNull(tree.Root.Right);
+        }
     }
+
+
 }
