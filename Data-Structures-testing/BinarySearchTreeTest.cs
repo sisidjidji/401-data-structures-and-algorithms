2c47619525f4eaab3ed035bb1870578f5b7fa552
sihem azibi <s.azibi@live.fr>
2020-05-25 21:06:56 -0500

    binary search tree add to empty  test passing

---
+++ Data-Structures-testing/BinarySearchTreeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+using Data_Structures.BinaryTree;
+
+namespace Data_Structures_testing
+{
+   public class BinarySearchTreeTest
+    {
+
+        [Fact]
+        public void can_Add_to_empty()
+        {
+            //Arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+
+
+            //assert
+            Assert.Null(tree.Root);
+            tree.AddNodeToTree(5);
+            Assert.NotNull(tree);
+        }
+    }
+}
