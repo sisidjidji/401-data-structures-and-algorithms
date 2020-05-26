6a3099d8d59ab9a6dd8b64fb0b4cb67f7cd6daf8
sihem azibi <s.azibi@live.fr>
2020-05-25 19:26:55 -0500

    inorder , preorder and postorder done tests working

---
+++ Data-Structures-testing/BinaryTreeTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+using Data_Structures.tree;
+using Microsoft.VisualStudio.TestPlatform.Common.DataCollection;
+
+namespace Data_Structures_testing
+{
+    public class BinaryTreeTests
+    {
+        [Fact]
+        public void can_PreOrder_Tree()
+        {
+            //Arrange
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Node<int> node1 = new Node<int>(5);
+            Node<int> node2 = new Node<int>(10);
+            Node<int> node3 = new Node<int>(7);
+            Node<int> node4 = new Node<int>(12);
+            // Act 
+            tree.Root= node1 ;
+            node1.Left = node2;
+            node1.Right = node3;
+            node2.Left = node4;
+
+            //Assert
+            Assert.Equal(new[] { 5, 10, 12, 7 }, tree.PreOrder(tree.Root));
+        }
+
+        [Fact]
+        public void can_InOrder_Tree()
+        {
+            //Arrange
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Node<int> node1 = new Node<int>(5);
+            Node<int> node2 = new Node<int>(10);
+            Node<int> node3 = new Node<int>(7);
+            Node<int> node4 = new Node<int>(12);
+            Node<int> node5 = new Node<int>(1);
+            // Act 
+            tree.Root = node1;
+            node1.Left = node2;
+            node1.Right = node3;
+            node2.Left = node4;
+            node2.Right = node5;
+
+            //Assert
+            Assert.Equal(new[] { 12, 10, 1,5,7}, tree.InOrder(tree.Root));
+        }
+
+
+        [Fact]
+        public void can_PostOrder_Tree()
+        {
+            //Arrange
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Node<int> node1 = new Node<int>(5);
+            Node<int> node2 = new Node<int>(10);
+            Node<int> node3 = new Node<int>(7);
+            Node<int> node4 = new Node<int>(12);
+            Node<int> node5 = new Node<int>(1);
+            // Act 
+            tree.Root = node1;
+            node1.Left = node2;
+            node1.Right = node3;
+            node2.Left = node4;
+            node2.Right = node5;
+
+            //Assert
+            Assert.Equal(new[] { 12, 1, 10, 7 ,5 }, tree.PostOrder(tree.Root));
+        }
+
+    }
+}
---
+++ Data-Structures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures.tree
+{
+     public class BinaryTree<T>
+    {
+        public Node<T> Root ;
+     
+
+        public IEnumerable<T> PreOrder(Node <T>Root)
+        {
+            yield return Root.Value;
+
+           if (Root == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+           if(Root.Left!=null)
+            {
+                foreach (T node in PreOrder(Root.Left))
+                {
+                    yield return node;
+                }
+               
+
+            }
+            if (Root.Right != null)
+            {
+                foreach (T node in PreOrder(Root.Right))
+                {
+                    yield return node;
+                }
+            }
+           
+           
+        }
+
+        public IEnumerable<T> InOrder(Node<T> Root)
+        {
+            
+
+            if (Root == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (Root.Left != null)
+            {
+                foreach (T node in InOrder(Root.Left))
+                    {
+                    yield return node;
+                    }
+            }
+
+            yield return Root.Value;
+
+            if (Root.Right != null)
+            {
+                foreach (T node in InOrder(Root.Right))
+                {
+                    yield return node;
+                }
+            }
+
+
+        }
+
+        public IEnumerable<T> PostOrder (Node<T> Root)
+        {
+            if (Root == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (Root.Left != null)
+            {
+                foreach (T node in PostOrder(Root.Left))
+                {
+                    yield return node;
+                }
+            }
+
+            
+            if (Root.Right != null)
+            {
+                foreach (T node in PostOrder(Root.Right))
+                {
+                    yield return node;
+                }
+            }
+
+            yield return Root.Value;
+
+        }
+
+    }
+    public class Node<T>
+    {
+        public Node( T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+
+        public T Value { get; set; }
+        public Node <T> Left { get; set; }
+        public Node <T> Right { get; set; }
+
+       
+    }
+}
