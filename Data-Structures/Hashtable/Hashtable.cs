fcf6d4e9286b93d3634f6abbb06a675485dc5873
sihem azibi <s.azibi@live.fr>
2020-06-14 22:28:02 -0500

    gethash function done

--- Data-Structures/Hashtable/Hashtable.cs
+++ Data-Structures/Hashtable/Hashtable.cs
@@ -8,7 +8,15 @@ namespace Data_Structures.Hashtable
     {
         public int GetHash(string key)
         {
-        
+            int hash = 0;
+
+                 for (int i= 0; i<key.Length; i++)
+            {
+                hash += key[i];
+            }
+
+            int index = hash * 599;
+            return index;
         }
     }
 }
