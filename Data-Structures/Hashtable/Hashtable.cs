eb41458fa2c8773c2b3f1ccfe72992cca2ccd5f4
sihem azibi <s.azibi@live.fr>
2020-06-14 22:08:12 -0500

    scaffolding

---
+++ Data-Structures/Hashtable/Hashtable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures.Hashtable
+{
+    class Hashtable
+    {
+        public int GetHash(string key)
+        {
+        
+        }
+    }
+}
