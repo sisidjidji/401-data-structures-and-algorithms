336acd7c03fe6c9a9d6f2d5fd75094d3d3e6f3dd
sihem azibi <s.azibi@live.fr>
2020-06-17 21:55:39 -0500

    two tests passing

--- Challenges.Test/JoinTableTest.cs
+++ Challenges.Test/JoinTableTest.cs
@@ -10,7 +10,7 @@ namespace Challenges.Test
     {
 
         [Fact]
-        public void test_join_table()
+        public void test_join_table_if_return_null()
         {
             Dictionary<string, string> hash1 = new Dictionary<string, string>();
             Dictionary<string, string> hash2 = new Dictionary<string, string>();
@@ -26,6 +26,53 @@ namespace Challenges.Test
             string[] expected1 = new string[] { "hi", "hello", null };
             string[] expected2 = new string[] { "green", "blue",null };
             string[] expected3 = new string[] { "pink,", "purple",null };
+
+            Assert.Contains(expected1, result);
+            Assert.Contains(expected2, result);
+            Assert.Contains(expected3, result);
+
+        }
+        [Fact]
+        public void test_join_table_if_contain_all_value()
+        {
+            Dictionary<string, string> hash1 = new Dictionary<string, string>();
+            Dictionary<string, string> hash2 = new Dictionary<string, string>();
+
+            hash1.Add("hi", "hello");
+            hash1.Add("green", "blue");
+            hash1.Add("pink,", "purple");
+
+            hash2.Add("hi", "bye");
+            hash2.Add("green", "dark green");
+            hash2.Add("pink,", "black");
+
+            List<string[]> result = LeftJoinTable.Leftjoin(hash1, hash2);
+
+            string[] expected1 = new string[] { "hi", "hello", "bye"};
+            string[] expected2 = new string[] { "green", "blue", "dark green"};
+            string[] expected3 = new string[] { "pink,", "purple", "black" };
+
+            Assert.Contains(expected1, result);
+            Assert.Contains(expected2, result);
+            Assert.Contains(expected3, result);
+
+        }
+        [Fact]
+        public void test_join_table_if_second_hashtable_empty()
+        {
+            Dictionary<string, string> hash1 = new Dictionary<string, string>();
+            Dictionary<string, string> hash2 = new Dictionary<string, string>();
+
+            hash1.Add("hi", "hello");
+            hash1.Add("green", "blue");
+            hash1.Add("pink,", "purple");
+
+
+            List<string[]> result = LeftJoinTable.Leftjoin(hash1, hash2);
+
+            string[] expected1 = new string[] { "hi", "hello", null };
+            string[] expected2 = new string[] { "green", "blue", null };
+            string[] expected3 = new string[] { "pink,", "purple", null };
 
             Assert.Contains(expected1, result);
             Assert.Contains(expected2, result);
