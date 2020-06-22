b02310c1e0411a60d2472706a1a580055278c4f9
sihem azibi <s.azibi@live.fr>
2020-06-21 20:15:08 -0500

    create a generic hash table that return  T value and node

---
+++ Data-Structures/Graphs/Graphs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data_Structures.Graphs
+{
+  
+    public class Graphs
+    {
+        public class Graph<T>
+        {
+            Dictionary<T, Dictionary<T, int>> Nodes;
+            public Graph()
+            {
+                Nodes = new Dictionary<T, Dictionary<T, int>>();
+            }
+           
+           
+
+ }
+   
